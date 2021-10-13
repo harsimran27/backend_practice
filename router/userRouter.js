d963f88f48305a7754da67436f35ee9d6bf33c04
Harsimran singh <simran.9971290436@gmail.com>
2021-10-13 18:11:40 +0530

    CRUD operation for the user

--- router/userRouter.js
+++ router/userRouter.js
@@ -5,28 +5,98 @@ let { bodyChecker, protectRoute } = require("./utilFunc");
 
 let fs = require("fs");
 
+userRouter.use(protectRoute);
 userRouter
-    .route("/")
-    .post(bodyChecker, createUser);
+    .route("/:id")
+    .get(bodyChecker, getUser)
+    .patch(bodyChecker, updateUser)
+    .delete(bodyChecker, deleteUser)
+
 
 userRouter
     .route("/")
     .get(protectRoute, getUsers)
+    .post(bodyChecker, createUser);
+
+async function createUser(req, res) {
+    try {
+        let user = await userModel.create(req.body);
+        res.status(200).json({
+            user: user,
+        })
+    } catch (err) {
+        res.statu(404).json({
+            message: err.message,
+        })
+    }
+}
 
 function getUsers(req, res) {
-    res.status(200).json({
-        message: content
-    });
+    try {
+        let user = await userModel.find();
+        res.status(200).json({
+            message: user,
+        })
+
+    } catch (err) {
+        res.statu(404).json({
+            message: err.message,
+        })
+    }
 }
 
-function createUser(req, res) {
-    let body = req.body;
-    content.push(body);
-    console.log(content);
-    fs.writeFileSync("./data.json", JSON.stringify(content));
-    res.json({
-        message: content
-    })
+async function getUser(req, res) {
+    try {
+        let { id } = req.params;
+        let user = await userModel.findById(id);
+        res.status(200).json({
+            message: user,
+        })
+
+    } catch (err) {
+        res.statu(404).json({
+            message: err.message,
+        })
+    }
 }
+
+async function updateUser(req, res) {
+    try {
+
+        let {id} = req.params;
+        let user = await userModel.findById(id);
+
+        for(let key in req.body){
+            user[key] = req.body[key];
+        }
+
+        await user.save();
+
+        res.status(200).json({
+            message: user,
+        })
+
+    } catch (err) {
+        res.statu(404).json({
+            message: err.message,
+        })
+    }
+}
+
+async function deleteUser(req, res) {
+    try {
+        let { id } = req.params;
+        let user = await userModel.findByIdAndDelete(id);
+        res.status(200).json({
+            message: user,
+        })
+
+    } catch (err) {
+        res.statu(404).json({
+            message: err.message,
+        })
+    }
+}
+
 
 module.exports = userRouter;
